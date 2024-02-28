e9ddff614cf1e733606b614e41d39b396e665b14
Jason Sherrick <jasonsherrick@gmail.com>
2024-02-28 02:38:20 -0600

    created dropdown navigation components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), mdx(), sitemap()]
 });
